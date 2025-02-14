3e08e99d1e6d7d2e637729d783d391474341cd7e
Jonathan Platteau <jonathan.platteau@yahoo.fr>
2025-02-14 16:47:04 +0100

    first deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env sh
 
+# First time before you deploy, you need to manually init the gh-pages branch
+
+# yarn build
+# cd dist
+# git init
+# git switch -c gh-pages
+
+# then you can launch this script
+
 # abort on errors
 set -e
 
@@ -12,6 +21,6 @@ cd dist
 git add -A
 git commit -m $cdate
 
-git push -u -f git@github.com:flibustier/pokemon-tcg-exchange.git gh-pages
+git push git@github.com:flibustier/pokemon-tcg-exchange.git gh-pages
 
 cd -
