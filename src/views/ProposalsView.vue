086e23f055f9840b73d8f7287b25d1e6de5b7b88
Jonathan Platteau <jonathan.platteau@yahoo.fr>
2025-05-06 20:02:53 +0200

    feat(proposals): redesign UI with grouping by ID and rarity

--- src/views/ProposalsView.vue
+++ src/views/ProposalsView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import cards from 'pokemon-tcg-pocket-database/dist/cards.json' with { type: 'json' }
 
 import { getProposals } from '@/services/api'
@@ -18,6 +18,40 @@ interface Proposal {
 
 const data = ref([] as Proposal[])
 
+type ProposalGroup = {
+  rarity: string
+  friendID: string
+  wantedCards: Card[]
+  givenCards: Card[]
+}
+
+const proposalsByRarityAndFriendId = computed(() => {
+  return data.value.reduce((acc: ProposalGroup[], proposal: Proposal): ProposalGroup[] => {
+    const rarity = proposal.card1.rarityCode
+    const friendID = proposal.friend_id
+
+    const existingGroup = acc.find(
+      (group) => group.rarity === rarity && group.friendID === friendID,
+    )
+    if (existingGroup) {
+      if (!existingGroup.wantedCards.includes(proposal.card1)) {
+        existingGroup.wantedCards.push(proposal.card1)
+      }
+      if (!existingGroup.givenCards.includes(proposal.card2)) {
+        existingGroup.givenCards.push(proposal.card2)
+      }
+    } else {
+      acc.push({
+        rarity,
+        friendID,
+        wantedCards: [proposal.card1],
+        givenCards: [proposal.card2],
+      })
+    }
+    return acc
+  }, [])
+})
+
 const findCard = (id: string) => {
   const [set, number] = id.split('-')
   return cards.find((card) => card.set === set && card.number === parseInt(number))
@@ -27,8 +61,8 @@ const formatFriendID = (friendID: string) => {
   return friendID.replace(/(\d{4})(\d{4})(\d{4})(\d{4})/, '$1-$2-$3-$4')
 }
 
-const exchangeTokenCost = (proposal: Proposal) => {
-  switch (proposal.card1.rarityCode) {
+const exchangeTokenCost = (rarity: string) => {
+  switch (rarity) {
     case 'R':
       return 120
     case 'RR':
@@ -51,34 +85,54 @@ onMounted(async () => {
 </script>
 
 <template>
-  <CenteredLayout>
-    <div v-if="data.length > 0" class="list">
+  <CenteredLayout style="padding: 2rem 0">
+    <template v-if="data.length > 0">
       <h2>You’ve got a match!</h2>
-      <div class="exchange-tile" v-for="(proposal, index) in data" :key="index">
-        <span class="tile-title">Friend ID : {{ formatFriendID(proposal.friend_id) }}</span>
-        <div class="card-container">
-          <img :src="`/images/cards/${proposal.card1.imageName}`" alt="Card 1" class="card-image" />
-        </div>
-        <div class="exchange-icon"></div>
-        <div class="card-container">
-          <img :src="`/images/cards/${proposal.card2.imageName}`" alt="Card 2" class="card-image" />
-        </div>
-        <div class="tile-footer">
-          <img
-            src="/images/TRADE_ITEM_130010.webp"
-            alt="Trade Token"
-            class="trade-token"
-            height="32"
-          />
-          <span class="token-count">{{ exchangeTokenCost(proposal) }}</span>
+
+      <div class="list">
+        <div
+          class="exchange-tile"
+          v-for="(group, index) in proposalsByRarityAndFriendId"
+          :key="index"
+        >
+          <span class="tile-title">Friend ID : {{ formatFriendID(group.friendID) }}</span>
+          <div class="cards">
+            <div class="card-container" v-for="(card, index) in group.wantedCards" :key="index">
+              <img
+                :src="`/images/cards/${card.imageName}`"
+                :alt="card.label.eng"
+                class="card-image"
+              />
+            </div>
+          </div>
+
+          <div class="exchange-icon"></div>
+          <div class="cards">
+            <div class="card-container" v-for="(card, index) in group.givenCards" :key="index">
+              <img
+                :src="`/images/cards/${card.imageName}`"
+                :alt="card.label.eng"
+                class="card-image"
+              />
+            </div>
+          </div>
+          <div class="tile-footer">
+            <img
+              src="/images/TRADE_ITEM_130010.webp"
+              alt="Trade Token"
+              class="trade-token"
+              height="32"
+            />
+            <span class="token-count">{{ exchangeTokenCost(group.rarity) }}</span>
+          </div>
         </div>
       </div>
-    </div>
-    <p v-else>
+    </template>
+    <h2 v-else>
       No matching proposals for now :(
       <br />
       Please come back later!
-    </p>
+    </h2>
   </CenteredLayout>
 </template>
 
@@ -86,8 +140,11 @@ onMounted(async () => {
 .list {
   width: 100%;
   display: flex;
-  flex-direction: column;
-  gap: 3rem;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 2rem;
+  margin-top: 3rem;
 }
 
 h2 {
@@ -97,19 +154,27 @@ h2 {
 .exchange-tile {
   position: relative;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: space-between;
-  padding: 32px 24px;
+  padding: 32px;
   background: linear-gradient(to bottom, #f1f9f1, #e8f3e8);
   border-radius: 16px;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
   max-width: 600px;
-  margin: 0 auto;
-  padding-bottom: 44px; /* Increased to accommodate footer */
+  min-width: 270px;
+  margin-bottom: 1rem;
+}
+
+.cards {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  max-width: 400px;
+  overflow-x: scroll;
 }
 
 .card-container {
-  width: 200px;
   height: 200px;
   display: flex;
   align-items: center;
@@ -135,15 +200,14 @@ h2 {
 }
 
 .exchange-icon::before {
-  content: '↔';
+  content: '⇅';
   color: #5f6368;
   font-size: 18px;
 }
 
 .tile-footer {
   position: absolute;
-  top: -12px;
-  left: 87px;
+  top: 515px;
   transform: none;
   background: white;
   padding: 4px 16px;
@@ -160,7 +224,6 @@ h2 {
 .tile-title {
   position: absolute;
   top: -12px;
-  right: 12px;
   transform: none;
   background: white;
   padding: 4px 16px;
