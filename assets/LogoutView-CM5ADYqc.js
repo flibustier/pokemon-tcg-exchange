c5f98384403ce2b7c52755459238fa43adc4cb06
Jonathan Platteau <jonathan.platteau@yahoo.fr>
2025-07-28 10:26:45 +0200

    07-28-2025

--- assets/LogoutView-CM5ADYqc.js
+++
@@ -1,1 +0,0 @@
-import{C as o}from"./CenteredLayout-Bb8cBMMC.js";import{d as t,a1 as a,l as n,o as r,m as s,e as l}from"./app-DmsZdftI.js";const m=t({__name:"LogoutView",setup(u){return a(),window.location.replace("/"),(i,e)=>(r(),n(o,null,{default:s(()=>e[0]||(e[0]=[l("h1",null,"Logging out...",-1)])),_:1,__:[0]}))}});export{m as default};
