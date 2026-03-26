0072fd2c66519850c20d22ab6f8c6e4a0174e487
Jonathan Platteau <jonathan.platteau@yahoo.fr>
2026-03-26 10:57:22 +0100

    feat(cards): add B2b

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -1,7 +1,10 @@
 for f in public/images/cards-by-set/*/*.webp; do 
     filename="$(basename "$f")"; 
-    # only last directory
     directory="$(basename "$(dirname "$f")")"; 
-    mkdir -p "public/images/cards-by-set/thumbnails/$directory"; 
-    magick "$f" -interlace plane -strip -quality 85 -resize 208x290 -sharpen 0x0.5 "public/images/cards-by-set/thumbnails/$directory/$filename"; 
+    thumbnail_path="public/images/cards-by-set/thumbnails/$directory/$filename"
+    
+    if [[ "$*" == *"-f"* ]] || [[ ! -f "$thumbnail_path" ]]; then
+        mkdir -p "public/images/cards-by-set/thumbnails/$directory"; 
+        magick "$f" -interlace plane -strip -quality 85 -resize 208x290 -sharpen 0x0.5 "$thumbnail_path"; 
+    fi
 done
