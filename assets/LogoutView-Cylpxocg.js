771774d103d4557bf58a2b9bba4abcf4ab8a561f
Jonathan Platteau <jonathan.platteau@yahoo.fr>
2026-07-07 23:09:08 +0200

    07-07-2026

---
+++ assets/LogoutView-Cylpxocg.js
@@ -0,0 +1,1 @@
+import{D as e,G as t,K as n,Z as r,_t as i,lt as a}from"./app-BThGp-y_.js";import{t as o}from"./CenteredLayout-BWraO0D-.js";var s=r({__name:`LogoutView`,setup(r){return e(),window.location.replace(`/`),(e,r)=>(a(),n(o,null,{default:i(()=>[...r[0]||=[t(`h1`,null,`Logging out...`,-1)]]),_:1}))}});export{s as default};
